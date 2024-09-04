d0dce84f5b8535c02957c1045ab11a722c3aac7b
papiblaq <yokwiraleon@gmail.com>
2024-09-04 14:14:29 +0300

    with null

--- financial_reporting_system/Program.cs
+++ financial_reporting_system/Program.cs
@@ -3,11 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
 var app = builder.Build();
 
-
-// syncfusion liscensing 
-
+// Syncfusion licensing
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBPh8sVXJyS0d+X1RPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9nSXlSc0ViWHhecnRVQWc=");
 
 // Configure the HTTP request pipeline.
